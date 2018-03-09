d5d2a2ba87f2a9341c3b761903985ffebae115fb
bmorawska <37190313+bmorawska@users.noreply.github.com>
2018-03-09 22:01:21 +0100

    Działa teraz na 8 + 4 bitach
    Mam tylko problem, bo nie wiem jak zrobić tak, żeby zakłócenie bitów parzystości też dało się poprawić.

--- HammingCodeFor1Error.cpp
+++ HammingCodeFor1Error.cpp
@@ -2,9 +2,17 @@
 #include <cstdlib>
 #include <ctime>
 #include <math.h>
+#include <fstream>
+#include <string>
+
+#define WORDLENGTH	(8)
+#define PARITYBITS	(4)
 
 using namespace std;
 
+/*
+ * Wypełnia tablicę jednowymiarową o wzkazanej długości zerami.
+ */
 void zeroArray (int *A, int length)
 {
 	for (int zeros = 0; zeros < length; zeros++)
@@ -13,61 +21,27 @@ void zeroArray (int *A, int length)
 	}
 }
 
-void printWord(int wordLength, int parityBits, const int *codeWord)
+/*
+ * Wypisuje na ekran konsoli tablicę jednowymiarową
+ * o wskazanej długości.
+ */
+void printWord(const int *message, int length)
 {
-	for (int i = 0; i < wordLength + parityBits; i++)
+	for (int i = 0; i < length; i++)
 	{
-		cout << codeWord[i] << " ";
+		cout << message[i] << " ";
 	}
 	cout << endl;
 }
 
-int main() {
-	int wordLength = 4;											//Długość słowa
-	int parityBits = 3;											//Bity parzystości
-	int word[4] = { 1, 0, 1, 1};								//Slowo
-	int codeWord[wordLength + parityBits];						//Slowo kodowe
-	int interference[3];
-	srand( time( NULL ) );
-
-	//pozbycie się śmieci z tablic
-	zeroArray(codeWord, (wordLength + parityBits));
-	zeroArray(interference, 3);
-
-	int H[parityBits][wordLength + parityBits];					//Macierz parzystości
-	H[0][0] = 1;	H[0][1] = 0;	H[0][2] = 1;	H[0][3] = 0;	H[0][4] = 1;	H[0][5] = 0;	H[0][6] = 1;
-	H[1][0] = 0;	H[1][1] = 1;	H[1][2] = 1;	H[1][3] = 0;	H[1][4] = 0;	H[1][5] = 1;	H[1][6] = 1;
-	H[2][0] = 0;	H[2][1] = 0;	H[2][2] = 0;	H[2][3] = 1;	H[2][4] = 1;	H[2][5] = 1;	H[2][6] = 1;
-
-	int G[wordLength][wordLength + parityBits];					//Macierz generująca słowa kodowe
-	G[0][0] = 1;	G[0][1] = 0;	G[0][2] = 0;	G[0][3] = 0;	G[0][4] = 1;	G[0][5] = 1;	G[0][6] = 0;
-	G[1][0] = 0;	G[1][1] = 1;	G[1][2] = 0;	G[1][3] = 0;	G[1][4] = 1;	G[1][5] = 0;	G[1][6] = 1;
-	G[2][0] = 0;	G[2][1] = 0;	G[2][2] = 1;	G[2][3] = 0;	G[2][4] = 0;	G[2][5] = 1;	G[2][6] = 1;
-	G[3][0] = 0;	G[3][1] = 0;	G[3][2] = 0;	G[3][3] = 1;	G[3][4] = 1;	G[3][5] = 1;	G[3][6] = 1;
-
-	//Wypisanie wiadomosci
-	cout << "Wiadomosc ma postac:                             ";
-	for (int m = 0; m < 4; m++)
-	{
-		cout << word[m] << " ";
-	}
-	cout << endl;
-
-	//Kodowanie słowa
-	for (int i = 0; i < wordLength + parityBits; i++)
-	{
-		for (int j = 0; j < wordLength; j++)
-		{
-			codeWord[i] += word[j] * G[j][i];
-		}
-		codeWord[i] %= 2;
-	}
-	cout << "Slowo kodowe ma postac:                          ";
-	printWord(wordLength, parityBits, codeWord);
-
-	//Wprowadzanie losowych zakłóceń
-	//int blad = rand() % (wordLength + parityBits);
-	int blad = rand() % (wordLength);
+/*
+ * Tworzy w słowie błąd na losowym bicie.
+ * Dlaczego nie zabezpiecza sam siebie???
+ */
+void createInterference(int *codeWord)
+{
+//	int blad = rand() % (WORDLENGTH + PARITYBITS);
+	int blad = rand() % (WORDLENGTH);
 	if (codeWord[blad] == 1)
 	{
 		codeWord[blad] = 0;
@@ -76,42 +50,50 @@ int main() {
 	{
 		codeWord[blad] = 1;
 	}
+	cout << "Blad jest na bicie: " << blad + 1 << " ale Ciiii! - to tajemnica. " << endl;
+}
 
-	cout << "Po transmisji narazonej na zaklocenia ma postac: ";
-	printWord(wordLength, parityBits, codeWord);
-
-	//Odnalezienie zakłócenia
-
-	for (int i = 0; i < parityBits; i++)
+/*
+ * Koduje słowo do postaci 12 bitowej, czyli takiej która po zakłóceniu
+ * maksymalnie jednego bita może zostać odtworzona.
+ */
+void encodeWord(int *codeWord, int* word, const int G[][WORDLENGTH + PARITYBITS])
+{
+	for (int i = 0; i < WORDLENGTH + PARITYBITS; i++)
 	{
-		for (int j = 0; j <  wordLength + parityBits; j++)
+		for (int j = 0; j < WORDLENGTH; j++)
 		{
-			interference[i] += H[i][j] * codeWord[j];
+			codeWord[i] += word[j] * G[j][i];
+		}
+		codeWord[i] %= 2;
+	}
+}
+
+/*
+ * Na podstawie iloczynu macierzy zakodowanego słowa (codeWord) i macierzy parzystości H
+ * tworzy macierz, która w sposób binarny zakodowuje, w któ©ej kolumnie nastąpiło zakłócenie.
+ * Na przykład [1, 0, 0] oznacza, że bład wystąpił w pierszej kolumnie czyli na zerowym bicie.
+ *
+ * Uwaga! Ta macierz chyba nie potrafi zabezpieczyć samej siebie więc to co jej wyjdzie
+ * 		  trzeba liczyć modulo 8. Wtedy działa ;-/
+ */
+void lookForInterference(int *interference, int *codeWord, const int H[][WORDLENGTH + PARITYBITS])
+{
+	for (int i = 0; i < PARITYBITS; i++)
+	{
+		for (int j = 0; j <  WORDLENGTH + PARITYBITS; j++)
+		{
+			interference[i] += codeWord[j] * H[i][j];
 		}
 		interference[i] %= 2;
 	}
-	cout << "Macierz wykrywajaca bledy wyglada tak:           ";
-	for (int m = parityBits - 1; m >= 0; m--)
-	{
-		cout << interference[m] << " ";
-	}
+}
 
-	cout << endl;
-
-	//Analiza zakłócenia
-	int bit = 0;
-	for (int n = 0; n < parityBits; n++)	//de facto zamiana z liczby binarnej na dziesietna
-	{
-		if (interference[n] == 1)
-		{
-			bit += pow(2, n);
-		}
-	}
-	bit -= 1; 								//wynika to z faktu, że tablice indeksujemy od zera, a nie od 1
-
-	cout << "Skad wniosek, ze przeklamanie znajduje sie na:   " << bit + 1<< " bicie." << endl;
-
-	//Poprawiamy zakłócenie
+/*
+ * Zamienia przekłamany bit na przeciwny.
+ */
+void fixError(int *codeWord, int bit)
+{
 	if (codeWord[bit] == 1)
 	{
 		codeWord[bit] = 0;
@@ -120,18 +102,95 @@ int main() {
 	{
 		codeWord[bit] = 1;
 	}
+}
 
-	cout << "Po poprawieniu slowo kodowe ma postac:           ";
-	printWord(wordLength, parityBits, codeWord);
-
-	//Przywrocenie tresci wiadomosci word
-	cout << "W efekcie wiadomosc wyglada nastepujaco          ";
-	int resetMsg[4];
-	for (int m = 0; m < 4; m++)
+/*
+ * Zamienia wartość z tablicy interference na liczbę w systemie dziesiętnym % 8 (dlaczego? -
+ * czytaj opis lookForInterference) i naprawia błąd.
+ */
+void analizeInterference(int *interference, int *codeWord)
+{
+	int bit = 0;
+	for (int n = 0; n < PARITYBITS; n++)	//de facto zamiana z liczby binarnej na dziesietna
 	{
-		resetMsg[m] = codeWord[m];
-		cout << resetMsg[m] << " ";
+		if (interference[n] == 1)
+		{
+			bit += pow(2, n);
+		}
+	}
+	bit -= 1; 								//wynika to z faktu, że tablice indeksujemy od zera, a nie od 1
+	bit %= 8;								//no bo niestety bity parzystości nie zabezpieczają samych siebie
+
+	cout << "Skad wniosek, ze przeklamanie znajduje sie na:   " << bit + 1<< " bicie." << endl;
+
+	fixError(codeWord, bit);
+}
+
+
+
+int main() {
+
+	srand( time( NULL ) );
+	int word[8] = { 1, 0, 1, 1, 1, 1, 1, 0};					//slowo
+	int codeWord[WORDLENGTH + PARITYBITS];						//slowo kodowe
+	int interference[PARITYBITS];								//tablica wskazująca zakłócenie
+	int resetMsg[WORDLENGTH];									//tablica, w której znajdzie się naprawione słowo
+
+	//Macierz parzystości H
+	const int H [PARITYBITS][WORDLENGTH + PARITYBITS] = { { 1, 0, 1, 0, 	1, 0, 1, 0, 	1, 0, 1, 0 },
+														  { 0, 1, 1, 0, 	0, 1, 1, 0, 	0, 1, 1, 0 },
+														  { 0, 0, 0, 1, 	1, 1, 1, 0, 	0, 0, 0, 1 },
+														  { 0, 0, 0, 0, 	0, 0, 0, 1, 	1, 1, 1, 1 } };
+	//Macierz generująca słowa kodowe
+	const int G[WORDLENGTH][WORDLENGTH + PARITYBITS]  = { { 1, 0, 0, 0, 	0, 0, 0, 0,		1, 0, 0, 0 },
+														  { 0, 1, 0, 0, 	0, 0, 0, 0,		0, 1, 0, 0 },
+														  { 0, 0, 1, 0, 	0, 0, 0, 0,		1, 1, 0, 0 },
+														  { 0, 0, 0, 1, 	0, 0, 0, 0,		0, 0, 1, 0 },
+														  { 0, 0, 0, 0, 	1, 0, 0, 0,		1, 0, 1, 0 },
+														  { 0, 0, 0, 0, 	0, 1, 0, 0,		0, 1, 1, 0 },
+														  { 0, 0, 0, 0, 	0, 0, 1, 0,		1, 1, 1, 0 },
+														  { 0, 0, 0, 0, 	0, 0, 0, 1,		0, 0, 0, 1 } };
+
+	//pozbycie się śmieci z tablic
+	zeroArray(codeWord, (WORDLENGTH + PARITYBITS));
+	zeroArray(interference, PARITYBITS);
+	zeroArray(resetMsg, WORDLENGTH);
+
+	//Wypisanie wiadomosci
+	cout << "Wiadomosc ma postac:                             ";
+	printWord(word, WORDLENGTH);
+
+	//Kodowanie słowa
+	encodeWord(codeWord, word, G);
+	cout << "Slowo kodowe ma postac:                          ";
+	printWord(codeWord, WORDLENGTH + PARITYBITS);
+
+	//Wprowadzanie losowych zakłóceń
+	createInterference(codeWord);
+	cout << "Po transmisji narazonej na zaklocenia ma postac: ";
+	printWord(codeWord, WORDLENGTH + PARITYBITS);
+
+	//Odnalezienie zakłócenia
+	lookForInterference(interference, codeWord, H);
+	cout << "Macierz wykrywajaca bledy wyglada tak:           ";
+	for (int m = PARITYBITS - 1; m >= 0; m--)	//Wypisywana od końca dla czytelności
+	{
+		cout << interference[m] << " ";
 	}
 	cout << endl;
 
+	//Poprawienie błędu
+	analizeInterference(interference, codeWord);
+	cout << "Po poprawieniu slowo kodowe ma postac:           ";
+	printWord(codeWord, WORDLENGTH + PARITYBITS);
+
+	//Przywrocenie tresci wiadomosci word
+	cout << "W efekcie wiadomosc wyglada nastepujaco          ";
+	for (int m = 0; m < WORDLENGTH; m++)
+	{
+		resetMsg[m] = codeWord[m];
+	}
+	printWord(codeWord, WORDLENGTH);
+
+	return 0;
 }
